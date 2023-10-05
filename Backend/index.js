7453bab54f91cb110916ae700a7d4b60c3249248
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2023-10-05 21:27:58 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,14 +15,7 @@ const Course = require('./Model/Course');
 
 require("dotenv").config({ path: "./.env" });
 
-const corsOptions = {
-    origin: 'https://eduvi-alpha.vercel.app',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -61,6 +54,13 @@ const verifyToken = (token) => {
         });
     });
 };
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://eduvi-alpha.vercel.app");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 
 const sendVerification = async ({ _id, email }, res) => {
 
